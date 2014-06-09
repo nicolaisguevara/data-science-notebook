7e42348fff739eadba8f02c4134d65d2d6ab9036
helio9cn <helio9cn@users.noreply.github.com>
2014-06-09 16:02:44 +0800

    Update iris.r

--- cran/randomForest/iris.r
+++ cran/randomForest/iris.r
@@ -1,4 +1,5 @@
 ## build random forest for iris dataset
+library(randomForest)
 
 ## load data iris
 data(iris)
@@ -11,16 +12,18 @@ train <- iris[ c(11:50, 61:100, 111:150), ]
 test <- iris[ c(1:10, 51:60, 101:110), ]
 
 ## build a random forest model
-rf <- randomForest(Species ~., data=train, importance=TRUE, do.trace=100)
+model.rf <- randomForest(Species ~., data=train, importance=TRUE, do.trace=100)
 
 ## look at model
-print(rf)
+print(model.rf)
 
 ## make predictions
-(iris.predict <- predict(rf, test))
+iris.prediction <- predict(model.rf, test)
 
 ## examine confusion matrix
-(t <- table(observed=test[,'Species'], predict=iris.predict))
+(t <- table(observed=test$Species, predict=iris.prediction))
 
 ## matrix in probability
 prop.table(t, 1)
+
+importance(model.rf)
