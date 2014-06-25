130905e4cac7b3902dcdcf242eb5d23c6d5cda2e
helio9cn <helio9cn@users.noreply.github.com>
2014-06-25 19:15:28 +0800

    Create example.r

---
+++ cran/ROCR/example.r
@@ -0,0 +1,15 @@
+## computing a simple ROC curve (x-axis: fpr, y-axis: tpr)
+library(ROCR)
+data(ROCR.simple)
+pred <- prediction( ROCR.simple$predictions, ROCR.simple$labels)
+perf <- performance(pred,"tpr","fpr")
+plot(perf)
+
+## precision/recall curve (x-axis: recall, y-axis: precision)
+perf1 <- performance(pred, "prec", "rec")
+plot(perf1)
+
+## sensitivity/specificity curve (x-axis: specificity,
+## y-axis: sensitivity)
+perf1 <- performance(pred, "sens", "spec")
+plot(perf1)
