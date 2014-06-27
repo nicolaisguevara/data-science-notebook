21579062e0bbf695b7ddd1e1f53a532ce831f301
helio9cn <helio9cn@users.noreply.github.com>
2014-06-27 15:34:45 +0800

    Update small_load.r

--- cran/kddcup2009/src/small_load.r
+++ cran/kddcup2009/src/small_load.r
@@ -8,7 +8,7 @@ df.small.labels_A <- read.csv("orange_small_train_appetency.labels", header=FALS
 
 ## set labels to categorical values
 df.small.labels_C$Churn <- factor(df.small.labels_C$Churn)
-df.small.labels_U$Upselling <- factor(df.small.labels_u$Upselling)
+df.small.labels_U$Upselling <- factor(df.small.labels_U$Upselling)
 df.small.labels_A$Appetency <- factor(df.small.labels_A$Appetency)
 
 ## combine data with labels
